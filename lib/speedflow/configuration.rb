19adf4a467abf513ad70f1dca2c7bc2c9d6113f0
Julien Breux <julien.breux@gmail.com>
2016-01-19 22:05:18 +0100

    Improve documentation

--- lib/speedflow/configuration.rb
+++ lib/speedflow/configuration.rb
@@ -1,16 +1,29 @@
 require 'yaml'
 
 module Speedflow
-  # Speedflow configuration
+  # Speedflow configuration module
   module Configuration
     extend self
 
     attr_accessor :project_path, :settings
 
+    # Default filename
     @filename = '.speedflow.yml'
+    # Project path
     @project_path = '.'
+    # Default settings
     @settings = {}
 
+    # Public: Load configuration file
+    #
+    # options - Hash of options.
+    #
+    # Examples
+    #
+    #    save
+    #    # => nill
+    #
+    # Returns nothing.
     def load(options = {})
       new_sets = YAML::load_file("#{@project_path}/#{@filename}").deep_symbolize_keys
       if options[:env] && newsets[options[:env].to_sym]
@@ -19,23 +32,60 @@ module Speedflow
       deep_merge!(@settings, new_sets)
     end
 
+    # Public: Save configuration file
+    #
+    # Examples
+    #
+    #    save
+    #    # => nill
+    #
+    # Returns nothing.
     def save
       File.open("#{@project_path}/#{@filename}", 'w') do |file|
         file.write @settings.deep_stringify_keys.to_yaml.gsub("---\n", '')
       end
     end
 
+    # Public: Check if configuration file exists
+    #
+    # Examples
+    #
+    #    exists?
+    #    # => True or False
+    #
+    # Returns if configuration file exists.
     def exists?
       File.exist?("#{@project_path}/#{@filename}")
     end
 
+    # Public: Deep merge data
+    #
+    # target - Hash of destination
+    # data   - Hash of source
+    #
+    # Examples
+    #
+    #    deep_merge!({}, {})
+    #    # => {}
+    #
+    # Returns configuration settings merged.
     def deep_merge!(target, data)
       merger = proc{|key, v1, v2|
         Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2 }
       target.merge! data, &merger
     end
 
-    def method_missing(name, *args, &block)
+    # Public: Check if configuration file exists
+    #
+    # name - Name of settings key
+    #
+    # Examples
+    #
+    #    method_missing(:foo)
+    #    # => "value"
+    #
+    # Returns configuration settings by name.
+    def method_missing(name)
       @settings[name.to_sym] ||
       fail(NoMethodError, "unknown configuration root #{name}", caller)
     end
