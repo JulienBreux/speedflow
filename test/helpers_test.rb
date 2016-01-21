a663ba432303512d4640512e12c1724fa961c00d
Julien Breux <julien.breux@gmail.com>
2016-01-21 18:17:39 +0100

    Fix coverage to full for helpers module

---
+++ test/helpers_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+require 'speedflow/helpers'
+
+class TestSpeedflowHelpers < Minitest::Test
+  include Speedflow::Helpers
+
+  def setup
+    $terminal = HighLine.new(nil, (@output = StringIO.new))
+  end
+
+  def test_notice
+    assert_equal(notice('test'), nil)
+    assert_equal(@output.string, "\e[0;94;49mtest\e[0m\n")
+  end
+
+  def test_success
+    assert_equal(success('test'), nil)
+    assert_equal(@output.string, "\e[0;92;49mtest\e[0m\n")
+  end
+
+  def test_error
+    assert_equal(error('test'), nil)
+    assert_equal(@output.string, "\e[0;91;49mtest\e[0m\n")
+  end
+end
